12749a1dd0247f9e312a4bf5d30b5ff90b598dfd
AlexisPerdomoD <dropdibemol@gmail.com>
2024-01-16 01:16:51 -0500

    feat: create post end point to add products by id to a cart

--- src/routes/carts/carts.routes.js
+++ src/routes/carts/carts.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import ProductManager from "../../productManager.js"
 import fs from "fs"
 import crypto from "crypto"
 
@@ -39,6 +40,45 @@ cartRouter.get("/:cid", async(req, res) =>{
     }else{
         res.status(404).send({message:"there's not cart created"})
     }
+})
+
+cartRouter.post("/:cid/product/:pid", async(req, res) => {
+    const pm = new ProductManager()
+    if(fs.existsSync(path)){
+        const carts = JSON.parse(await fs.promises.readFile(path, "utf-8"))
+        const cIndex = carts.findIndex(c => c.id === req.params.cid)
+        const product = await pm.getProductById(req.params.pid)
+        if(cIndex !== -1 && product){
+            if(carts[cIndex].find(p => p.id === product.id)){
+                carts[cIndex].forEach(p => {
+                    p.id === product.id && p.quantity++
+                })
+                await fs.promises.writeFile(path, JSON.stringify(carts))
+                res.send({
+                    message:"product updated in cart properly",
+                    content:carts[cIndex]
+                })
+
+            }else{
+                carts[cIndex].push({
+                    id:product.id,
+                    quantity: 1
+                })
+
+                await fs.promises.writeFile(path, JSON.stringify(carts))
+                res.send({
+                    message:"product added in cart properly",
+                    content:carts[cIndex]
+                })
+
+            }
+        }else{
+            res.status(406).send({message:"not cart or product found, check ids in your request"})
+        }
+    }else{
+        res.status(404).send({message:"there's not cart created"})
+    }
+    
 })
 
 
