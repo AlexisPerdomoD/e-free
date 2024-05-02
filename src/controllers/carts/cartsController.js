105fa53f9413d8431c8b157ba84c3f69687807cf
AlexisPerdomoD <dropdibemol@gmail.com>
2024-05-02 16:56:09 -0500

    feat: impplement error manager to cart endpoints

--- src/controllers/carts/cartsController.js
+++ src/controllers/carts/cartsController.js
@@ -1,50 +1,55 @@
 import CartMannagerM from "../../dao/db/CartMannagerM.js"
-
+import em, { ErrorCode } from "../../utils/error.manager.js"
 const cM = new CartMannagerM()
 
-
-
-export async function getCartController(req, res){
+export async function getCartController(req, res) {
     const response = await cM.getCartById(req.session.cart)
-    
-    if(!response) return res.status(404).send({error:"no cart found"})
-    response.error
-    ? res.status(response.status).send({message: response?.message || "something went wrong"})
-    : res.send(response)
+    if (cart === null)
+        throw em.createError({
+            name: "CastError",
+            status: 404,
+            message: "not cart with the given id",
+            code: ErrorCode.GENERAL_USER_ERROR,
+        })
+
+    return res.send(response)
 }
 
-export async function updateCartController(req, res){
-    if(isNaN(req.body.quantity === undefined || +req.body.quantity) || +req.body.quantity < 0) return res.status(400).send({
-        message:"you are not adding any product, bad request", 
-        error:true
-    })
-    const response = await cM.updateCartProduct(req.session.cart, req.params.pid, req.body.quantity)
-    response.error
-    ? res.status(response.status).send({message: response?.message || "something went wrong"})
-    : res.send(response)
+export async function updateCartController(req, res) {
+    if (
+        isNaN(req.body.quantity === undefined || +req.body.quantity) ||
+        +req.body.quantity < 0
+    )
+        throw em.createError({
+            message: "you are not adding any product, bad request",
+            CastError: "Bad Request",
+            code: ErrorCode.GENERAL_USER_ERROR,
+            status: 400,
+        })
+    const response = await cM.updateCartProduct(
+        req.session.cart,
+        req.params.pid,
+        req.body.quantity
+    )
+    return res.send(response)
 }
 
-export async function deleteProductFromCartController(req, res){
-    const response = await cM.deleteProductfromCart(req.session.cart, req.params.pid)
-
-    response.error
-    ? res.status(response.status).send(response)
-    : res.send(response)
+export async function deleteProductFromCartController(req, res) {
+    const response = await cM.deleteProductfromCart(
+        req.session.cart,
+        req.params.pid
+    )
+    return res.send(response)
 }
 
-export async function deleteCart(req, res){
+export async function deleteCart(req, res) {
     const response = await cM.deleteCartById(req.params.cid)
-
-    response.error
-    ? res.status(response.status).send(response)
-    : res.send(response)
+    return res.send(response)
 }
 
-export async function checkOutCart(req, res){
+export async function checkOutCart(req, res) {
     const response = await cM.cartCheckOut(req.session.cart)
-    response.error
-    ? res.status(response.status).send({message: response?.message || "something went wrong"})
-    : res.send({...response, user: req.session.ussername})
+    return res.send({ ...response, user: req.session.ussername })
 }
 
 // cartRouter.post("/", async (req, res) =>{
--- src/dao/db/CartMannagerM.js
+++ src/dao/db/CartMannagerM.js
@@ -1,165 +1,203 @@
 import cartModel from "../models/cart.model.js"
 import ProductMannagerM from "./ProductMannagerM.js"
 import TicketMannager from "./TicketMannager.js"
+import em, { ErrorCode } from "../../utils/error.manager.js"
 const pm = new ProductMannagerM()
 
-export default class CartMannagerM{
-    getCartById(id){
-        try {
-            return cartModel.findOne({_id: id})
-        } catch (error) {
-            return {
-                message:"there was a problem looking for the cart with the id: "+ id,
-                error: true,
-                status:500
-            }
-        }
+export default class CartMannagerM {
+    getCartById(id) {
+        return cartModel.findOne({ _id: id })
     }
-    async deleteCartById(id){
-        const response = await cartModel.findByIdAndDelete(id)
-        try {
-            return {
-                message: response ? "cart eliminated properly" : "cart not found",
-                status: response ? 200 : 404
-            }
-        } catch (error) {
-            return {
-                message:"there was a problem looking for the cart with the id: "+ id,
-                status:500,
-                error:error
-            }
-        }
+
+    async deleteCartById(id) {
+        return cartModel.findByIdAndDelete(id)
     }
-    async addCart(){
+    async addCart() {
         try {
-            let newCart =  new cartModel()
+            let newCart = new cartModel()
             newCart = await newCart.save()
             return {
-                message:"cart properly added, id: "+ newCart._id,
-                content: newCart}
-        } catch (error) {
-            return {
-                message:`there was a problem creating the cart`,
-                error: error,
-                status:500
+                message: "cart properly added, id: " + newCart._id,
+                content: newCart,
             }
+        } catch (error) {
+            if (error instanceof Error && error.name === "CastError") {
+                throw em.createError({
+                    name: error.name,
+                    message: error.message,
+                    code: ErrorCode.GENERAL_USER_ERROR,
+                    status: 400,
+                    cause: "client problem",
+                })
+            }
+            throw em.createError({
+                name: "internal error server",
+                message: "somethinng when wrong creating the cart",
+                status: 500,
+                code: ErrorCode.DATABASE_ERROR,
+            })
         }
     }
-    async deleteProductfromCart(cId, pId){
-        try {
-            const product = await pm.getProductById(pId)
-            if (product.error) return product
+    async deleteProductfromCart(cId, pId) {
+        const product = await pm.getProductById(pId)
+        if (product === null)
+            throw em.createError({
+                status: 404,
+                message: "the product with the given id does not exist",
+                name: "CastError",
+                code: ErrorCode.GENERAL_USER_ERROR,
+            })
+        const cart = (await cartModel.find({ _id: cId }))[0]
+        if (cart === null)
+            throw em.createError({
+                name: "CastError",
+                status: 404,
+                message: "not cart with the given id",
+                code: ErrorCode.GENERAL_USER_ERROR,
+            })
 
-            const cart = (await cartModel.find({_id:cId}))[0]
-            if (cart === null) return {error:true, status:404, message:"not cart with the given id"}
-
-            if (!cart.products.find(product => product.product.toString() === pId)) return {message:"not product with the given id", status:400, error:true}
-
-            cart.products = cart.products.filter(product => product.product.toString() !== pId)
-            const response = await cartModel.updateOne({_id: cId}, {$set: {products: cart.products}})
-            return {
-                message:"product properly deleted with the id "+ pId,
-                content: response
-            }
-        } catch (error) {
-            return {
-                message:`there was a problem deleting the product in the cart with id: ${cId}`,
-                error,
-                status:500
-            }
+        if (
+            !cart.products.find((product) => product.product.toString() === pId)
+        )
+            throw em.createError({
+                name: "CastError",
+                status: 404,
+                code: ErrorCode.GENERAL_USER_ERROR,
+                message: "the product was not found inside the cart",
+            })
+        cart.products = cart.products.filter(
+            (product) => product.product.toString() !== pId
+        )
+        const response = await cartModel.updateOne(
+            { _id: cId },
+            { $set: { products: cart.products } }
+        )
+        return {
+            message: "product properly deleted with the id " + pId,
+            content: response,
         }
     }
-    async updateCartProduct(cId, pId, quantity = 1){
-        try {
-            const product = await pm.getProductById(pId)
-            //return product error 
-            if (product.error) return product
+    async updateCartProduct(cId, pId, quantity = 1) {
+        const product = await pm.getProductById(pId)
+        //return product error
+        if (product === null)
+            throw em.createError({
+                name: "CastError",
+                message: "the product with the given id does not exist",
+                status: 404,
+                code: ErrorCode.GENERAL_USER_ERROR,
+            })
 
-            const cart = (await cartModel.find({_id:cId}))[0]
-            if (cart === null) return {error:true, status:404, message:"not cart with the given id"}
+        const cart = (await cartModel.find({ _id: cId }))[0]
+        if (cart === null)
+            throw em.createError({
+                name: "CastError",
+                status: 404,
+                message: "not cart with the given id",
+                code: ErrorCode.GENERAL_USER_ERROR,
+            })
 
-            const existingProduct = cart.products.find(item => item.product.toString() === pId)
-            quantity = +quantity
+        const existingProduct = cart.products.find(
+            (item) => item.product.toString() === pId
+        )
+        quantity = +quantity
+        if (!existingProduct && quantity === 0)
+            throw em.createError({
+                message: `the product id:${pId} is not in the cart list`,
+                name: "CastError",
+                status: 404,
+                code: ErrorCode.GENERAL_USER_ERROR,
+            })
 
-            if (!existingProduct && quantity === 0) return  {
-                message:`the product id:${pId} is not in the cart list`, 
-                status:400,
-                error:true
-            }
-            
-            if (existingProduct){
-                cart.products = quantity === 0 
-                ? cart.products.filter(item => item.product.toString() !== pId)
-                : existingProduct.quantity = quantity
-            }else{
-                let p =  await pm.getProductById(pId)
-                cart.products.push({product: p.content._id, quantity})
-            }
-            const response = await cartModel.updateOne({_id: cId}, {$set: {products: cart.products}})
-            return {
-                message:"cart properly updated",
-                content: response
-            }
-        } catch (error) {
-            return {
-                message:`there was a problem updating the cart id: ${cId}`,
-                error: error,
-                status:500
-            }
+        if (existingProduct) {
+            cart.products =
+                quantity === 0
+                    ? cart.products.filter(
+                          (item) => item.product.toString() !== pId
+                      )
+                    : (existingProduct.quantity = quantity)
+        } else {
+            let p = await pm.getProductById(pId)
+            cart.products.push({ product: p.content._id, quantity })
+        }
+        const response = await cartModel.updateOne(
+            { _id: cId },
+            { $set: { products: cart.products } }
+        )
+        return {
+            message: "cart properly updated",
+            content: response,
         }
     }
-    async cartCheckOut(cId){
-        try {
-            let cart = (await cartModel.find({_id:cId}))[0]
-            if(cart.products.length <= 0)return {status:400, error:true, message:"the cart does not have any product yet"}
+    async cartCheckOut(cId) {
+        let cart = (await cartModel.find({ _id: cId }))[0]
+        if (cart.products.length <= 0)
+            throw em.createError({
+                status: 400,
+                code: ErrorCode.GENERAL_USER_ERROR,
+                message: "the cart does not have any product yet",
+                name: "empty cart",
+            })
 
-            const ticketInfo = {
-                amount:0,
-                products:[]
+        const ticketInfo = {
+            amount: 0,
+            products: [],
+        }
+
+        for (const p of cart.products) {
+            cart = (await cartModel.find({ _id: cId }))[0]
+            let product = await pm.getProductById(p.product)
+            if (product.status === 404)
+                throw em.createError({
+                    name: "fatal error",
+                    message:
+                        "there was a product id in this cart that is not in the database",
+                    status: 404,
+                    code: ErrorCode.GENERAL_USER_ERROR,
+                })
+            product = product.content
+
+            if (product.stock >= p.quantity) {
+                ticketInfo.amount += p.quantity * product.price
+
+                ticketInfo.products.push({
+                    product_name: product.title,
+                    quantity: p.quantity,
+                    product_amount: p.quantity * product.price,
+                    price: product.price,
+                })
+
+                await pm.updateProduct(product._id, {
+                    stock: product.stock - p.quantity,
+                })
+
+                const newProducts = cart.products.filter(
+                    (pr) => pr.product.toString() !== product._id.toString()
+                )
+                await cartModel.findByIdAndUpdate(
+                    cId,
+                    { products: newProducts },
+                    { new: true }
+                )
             }
+        }
 
-            for (const p of cart.products) {
-                cart = (await cartModel.find({_id:cId}))[0]
-                let product = await pm.getProductById(p.product)
-                if(product.status === 404) throw new Error({error:true, message:"there was a product id in this cart that is not in the database"})
-                product = product.content
-                
-                if(product.stock >= p.quantity){
-                    ticketInfo.amount += p.quantity * product.price
-
-                    ticketInfo.products.push({
-                        product_name: product.title,
-                        quantity : p.quantity,
-                        product_amount: p.quantity * product.price,
-                        price: product.price
-                    })
-
-                    await pm.updateProduct(product._id, {stock:product.stock -p.quantity})
-
-                    const newProducts = cart.products.filter(pr => pr.product.toString() !== product._id.toString())
-                    await cartModel.findByIdAndUpdate(
-                        cId,
-                        {products:newProducts},
-                        {new:true}
-                    )
-                }
-            }
-
-            if(ticketInfo.products.length <= 0)return{ticket:null, message:"there is not stock for any of your products right now"}
-
-            const tm = new TicketMannager()
-            const ticket = await tm.addTicket(ticketInfo)
-            cart = (await cartModel.find({_id:cId}))[0]
+        if (ticketInfo.products.length <= 0)
             return {
-                message: cart.products.length > 0 ? "there were some missing stock for some of your products, they are still on the cart for the future" : "all products were properly included in your order",
-                ticket
+                ticket: null,
+                message:
+                    "there is not stock for any of your products right now",
             }
-        } catch (error) {
-            return {
-                message:`there was a problem checking out the cart id: ${cId}`,
-                error: error,
-                status:500
-            }
+
+        const tm = new TicketMannager()
+        const ticket = await tm.addTicket(ticketInfo)
+        cart = (await cartModel.find({ _id: cId }))[0]
+        return {
+            message:
+                cart.products.length > 0
+                    ? "there were some missing stock for some of your products, they are still on the cart for the future"
+                    : "all products were properly included in your order",
+            ticket,
         }
     }
 }
