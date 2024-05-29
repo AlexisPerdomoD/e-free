b53d06bbc330b1ce9133cc43e4ea28e49e504baf
alexisperdomod <dropdibemol@gmail.com>
2024-05-29 17:11:19 -0500

    fix: isUsser middleware only call error when is an admin or not authenticated session

--- src/utils/users.midleware.js
+++ src/utils/users.midleware.js
@@ -6,7 +6,7 @@ export function isAdm(req, _res, next) {
     return next()
 }
 export function isUsser(req, _res, next) {
-    if (!req.session.ussername || req.session.rol !== "usser") return errorManager.generateAuthorizationError()
+    if (!req.session.ussername || req.session.rol === "admin") return errorManager.generateAuthorizationError()
     return next()
 }
 export function isLogged(req, _res, next) {
