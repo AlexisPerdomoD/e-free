414428b7822d9cc97924ea5f1b736165373dfc69
alexisperdomod <dropdibemol@gmail.com>
2024-06-03 09:51:21 -0500

    test: implement unit test for products mannager

--- src/dao/db/ProductMannagerM.js
+++ src/dao/db/ProductMannagerM.js
@@ -1,67 +1,63 @@
-import productModel from "../models/product.model.js"
+import productModel from "../models/product.model.js";
 
 export default class ProductMannagerM {
-    async getProductsPaginate(querys) {
-        const options = { limit: 10, page: 1 }
-        if (querys.limit) {
-            options.limit = +querys.limit
-            delete querys.limit
-        }
-        if (querys.page) {
-            options.page = +querys.page
-            delete querys.page
-        }
-        if (querys.sort) {
-            let sort = querys.sort
-            let toOrder = querys.to ? querys.to : "price"
-            delete querys.to
-            delete querys.sort
-            options.sort = {}
-            options.sort[toOrder] = +sort || sort
-        }
-        let data = await productModel.paginate(querys, options)
-        const response = { status: "success" }
-        response.payload = data.totalDocs
-        response.products = data.docs
-        response.totalPages = data.totalPages
-        response.page = data.page
-        response.hasPrevPage = data.hasPrevPage
-        response.hasNextPage = data.hasNextPage
-        response.nextPage = data.nextPage
-        response.prevPage = data.prevPage
+  async getProductsPaginate(querys) {
+    const options = { limit: 10, page: 1 };
+    if (querys?.limit) {
+      options.limit = +querys.limit;
+      delete querys.limit;
+    }
+    if (querys?.page) {
+      options.page = +querys.page;
+      delete querys.page;
+    }
+    if (querys?.sort) {
+      let sort = querys.sort;
+      let toOrder = querys.to ? querys.to : "price";
+      delete querys.to;
+      delete querys.sort;
+      options.sort = {};
+      options.sort[toOrder] = +sort || sort;
+    }
+    let data = await productModel.paginate(querys, options);
+    const response = { status: "success" };
+    response.payload = data.totalDocs;
+    response.products = data.docs;
+    response.totalPages = data.totalPages;
+    response.page = data.page;
+    response.hasPrevPage = data.hasPrevPage;
+    response.hasNextPage = data.hasNextPage;
+    response.nextPage = data.nextPage;
+    response.prevPage = data.prevPage;
 
-        return response
-    }
-    async getProductById(id) {
-        return {
-            message: "product found",
-            content: await productModel.findById(id),
-        }
-    }
-    async deleteProductById(id) {
-        const response = await productModel.findByIdAndDelete(id)
-        return {
-            message:
-                "product " + (response ? " eliminated properly" : " not found"),
-            content: response,
-        }
-    }
-    async addProduct(product) {
-        let newProduct = new productModel(product)
-        newProduct = await newProduct.save()
-        return {
-            message: "product properly added, id: " + newProduct._id,
-            content: newProduct,
-        }
-    }
-    async updateProduct(id, updates) {
-        let response = await productModel.updateOne(
-            { _id: id },
-            { $set: updates }
-        )
-        return {
-            message: "product properly updated",
-            content: response,
-        }
-    }
+    return response;
+  }
+  async getProductById(id) {
+    return {
+      message: "product found",
+      content: await productModel.findById(id),
+    };
+  }
+  async deleteProductById(id) {
+    const response = await productModel.findByIdAndDelete(id);
+    return {
+      message: "product " + (response ? " eliminated properly" : " not found"),
+      content: response,
+    };
+  }
+  async addProduct(product) {
+    let newProduct = new productModel(product);
+    newProduct = await newProduct.save();
+    return {
+      message: "product properly added, id: " + newProduct._id,
+      content: newProduct,
+    };
+  }
+  async updateProduct(id, updates) {
+    await productModel.updateOne({ _id: id }, { $set: updates });
+    return {
+      message: "product properly updated",
+      content: await productModel.findById(id),
+    };
+  }
 }
---
+++ test/unit_test/productsMannager.test.js
@@ -0,0 +1,88 @@
+import assert from "assert"
+import ProductManager  from "../../src/dao/db/ProductMannagerM.js"
+import connectDB from "../../src/utils/connectDB.js"
+import logger from "../../src/config/winston.config.js"
+import dotenvConfig from "../../src/config/dotenv.config.js"
+const pm = new ProductManager()
+before( (done) => {
+    connectDB(dotenvConfig.db)
+        .then(_e => done())
+        .catch(e => {
+            logger.error('error trying to connect db')
+            done(e)    
+        }) 
+})
+
+describe("Testing Product Manager", async()=>{
+           const productInfo = {
+            title: "test",
+            description: "test",
+            price: 10,
+            code: "test",
+            stock: 10,
+            category: "test"
+        }
+    let product 
+    it("Testing getProducts function with no query", async () =>{
+        const response = await pm.getProductsPaginate()
+        assert.ok(response)
+        assert.strictEqual(response.status, "success")
+        // default limit is 10 and page is 1
+        assert.strictEqual(response.page, 1)
+        assert.strictEqual(response.products.length, 10)
+    })
+    it("Testing getProducts function with query including category", async () =>{
+        const query = {category: "sides"}
+        const response = await pm.getProductsPaginate(query)
+        assert.ok(response)
+        assert.strictEqual(response.status, "success")
+        for (const product of response.products) {
+            assert.strictEqual(product.category, "sides")
+        }
+    })
+    it("Testing getProducts function with query including limit and page", async () =>{
+        const query = {limit: 5, page: 2}
+        const response = await pm.getProductsPaginate(query)
+        assert.ok(response)
+        assert.strictEqual(response.status, "success")
+        assert.strictEqual(response.page, 2)
+        assert.strictEqual(response.products.length, 5)
+    })
+    it("testing addProduct function", async () => {
+        const response = await pm.addProduct(productInfo)
+        assert.ok(response)
+        assert.strictEqual(response.content.title, productInfo.title)
+        assert.strictEqual(response.content.description, productInfo.description)
+        assert.strictEqual(response.content.price, productInfo.price)
+        assert.strictEqual(response.content.code, productInfo.code)
+        assert.ok(response.message)
+        product = response.content
+    })
+    it("testing getProductById function", async () => {
+        const response = await pm.getProductById(product._id)
+        assert.ok(response)
+        assert.strictEqual(response.content.title, productInfo.title)
+        assert.strictEqual(response.content.description, productInfo.description)
+        assert.strictEqual(response.content.price, productInfo.price)
+        assert.strictEqual(response.content.code, productInfo.code)
+        assert.ok(response.message)
+    })
+    it("testing updateProduct function", async () => {
+        const response = await pm.updateProduct(product._id, {title: "test2"})
+        assert.ok(response)
+        assert.strictEqual(response.content.title, "test2")
+        assert.strictEqual(response.content.description, productInfo.description)
+        assert.strictEqual(response.content.price, productInfo.price)
+        assert.strictEqual(response.content.code, productInfo.code)
+        assert.ok(response.message)
+    })
+    it("delete one product", async () => {
+        const response = await pm.deleteProductById(product._id)
+        assert.ok(response)
+        assert.strictEqual(response.content.title, "test2")
+        assert.strictEqual(response.content.description, productInfo.description)
+        assert.strictEqual(response.content.price, productInfo.price)
+        assert.strictEqual(response.content.code, productInfo.code)
+        assert.ok(response.message)
+    })
+})
