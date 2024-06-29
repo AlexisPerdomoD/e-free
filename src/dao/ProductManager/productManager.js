95209e8f34a344587d260cede8e0bcf5d1699996
alexisperdomod <dropdibemol@gmail.com>
2024-06-29 17:31:05 -0500

    refactor: productMannager fs persistence

--- src/dao/ProductManager/productManager.js
+++ src/dao/ProductManager/productManager.js
@@ -1,8 +1,8 @@
-import fs from "fs"
-import crypto from "crypto"
-import { checkLocalDbFile as checkDb } from "../../utils/utils.js"
-import envOptions from "../../config/dotenv.config.js"
-import em, { ErrorCode } from "../../utils/error.manager.js"
+import fs from 'fs'
+import crypto from 'crypto'
+import { checkLocalDbFile as checkDb } from '../../utils/utils.js'
+import envOptions from '../../config/dotenv.config.js'
+import em, { ErrorCode } from '../../utils/error.manager.js'
 
 export default class ProductManager {
     constructor(path) {
@@ -11,23 +11,21 @@ export default class ProductManager {
     async getProductsPaginate(querys) {
         if (fs.existsSync(this.path))
             return em.createError({
-                name: "CastError",
+                name: 'CastError',
                 code: ErrorCode.DATABASE_ERROR,
-                message: "there is not file found",
-                status: 500,
+                message: 'there is not file found',
+                status: 500
             })
-        const { limit, page, sort, category } = querys
+        let { limit, page, sort, category } = querys
         const { products, payload } = await checkDb(this.path)
-        const response = { products, payload, status: "success" }
+        const response = { products, payload, status: 'success' }
         response.totalPages = 1
         response.nextPage = null
         response.prevPage = null
         response.hasPrevPage = false
         response.hasNextPage = false
-        if (sort && sort === "1")
-            response.products.sort((a, b) => a.price - b.price)
-        if (sort && sort === "-1")
-            response.products.sort((a, b) => b.price - a.price)
+        if (sort && sort === '1') response.products.sort((a, b) => a.price - b.price)
+        if (sort && sort === '-1') response.products.sort((a, b) => b.price - a.price)
         if (!limit || limit > 100) limit = 100 //dont want anything broken
         if (category) {
             response.products = products.filter((p) => (p.category = category))
@@ -40,9 +38,7 @@ export default class ProductManager {
             if (page && page - 1 > 1) {
                 response.products = response.products.slice(
                     (page - 1) * limit - 1,
-                    page * limit > products.length - 1
-                        ? products.length - 1
-                        : page * limit
+                    page * limit > products.length - 1 ? products.length - 1 : page * limit
                 )
             } else {
                 response.products = response.products.slice(0, limit - 1)
@@ -50,46 +46,33 @@ export default class ProductManager {
         }
         if (page && page > 1) {
             response.hasPrevPage = true
-            response.prevPage = envOptions.host + "/?page=" + parseInt(page) - 1
+            response.prevPage = envOptions.host + '/?page=' + parseInt(page) - 1
         }
         if (page && page < response.totalPages) {
             response.hasNextPage = true
-            response.nextPage = envOptions.host + "/?page=" + parseInt(page) + 1
+            response.nextPage = envOptions.host + '/?page=' + parseInt(page) + 1
         }
 
-        if (category && response.prevPage)
-            response.prevPage += `&20category=${category}&20`
-        if (category && response.nextPage)
-            response.nextPage += `&20category=${category}&20`
-        if (limit && response.prevPage)
-            response.prevPage += `&20limit=${limit}&20`
-        if (limit && response.nextPage)
-            response.nextPage += `&20limit=${limit}&20`
+        if (category && response.prevPage) response.prevPage += `&20category=${category}&20`
+        if (category && response.nextPage) response.nextPage += `&20category=${category}&20`
+        if (limit && response.prevPage) response.prevPage += `&20limit=${limit}&20`
+        if (limit && response.nextPage) response.nextPage += `&20limit=${limit}&20`
         return response
     }
-    async addProduct({
-        title,
-        description,
-        price,
-        category,
-        thumbnail,
-        code,
-        stock,
-        status = true,
-    }) {
+    async addProduct({ title, description, price, category, thumbnail, code, stock, status = true }) {
         try {
             const m = await checkDb(this.path)
             // VALIDACIONES
             if (m.products.find((p) => p.code === code)) {
                 throw em.createError({
                     message:
-                        "code already used in the catalogo, in order to add a new product must have an unique code",
-                    name: "CastError",
+                        'code already used in the catalogo, in order to add a new product must have an unique code',
+                    name: 'CastError',
                     code: ErrorCode.GENERAL_USER_ERROR,
-                    status: 400,
+                    status: 400
                 })
             } else {
-                const id = crypto.randomBytes(16).toString("hex")
+                const id = crypto.randomBytes(16).toString('hex')
                 const newProduct = {
                     id,
                     title,
@@ -99,26 +82,23 @@ export default class ProductManager {
                     thumbnail,
                     code,
                     stock,
-                    status,
+                    status
                 }
                 m.products.push(newProduct)
                 // AGREGAR EL PRODUCTO
-                await fs.promises.writeFile(
-                    this.path,
-                    JSON.stringify({ products: m.products })
-                )
+                await fs.promises.writeFile(this.path, JSON.stringify({ products: m.products }))
                 return {
                     message: `product ${newProduct.code} correctly added`,
-                    content: newProduct,
+                    content: newProduct
                 }
             }
         } catch (error) {
             throw em.createError({
-                name: error.name || "Error",
-                message: error.message || "Error trying to add product",
+                name: error.name || 'Error',
+                message: error.message || 'Error trying to add product',
                 code: ErrorCode.DATABASE_ERROR,
                 status: 500,
-                cause: error.cause || "",
+                cause: error.cause || ''
             })
         }
     }
@@ -129,17 +109,17 @@ export default class ProductManager {
             return res
                 ? res
                 : em.createError({
-                      name: "CastError",
-                      message: "product not found",
-                      status: 404,
+                      name: 'CastError',
+                      message: 'product not found',
+                      status: 404
                   })
         } catch (error) {
             throw em.createError({
-                name: error.name || "Error",
-                message: error.message || "Error trying to get a product",
+                name: error.name || 'Error',
+                message: error.message || 'Error trying to get a product',
                 code: ErrorCode.DATABASE_ERROR,
                 status: 500,
-                cause: error.cause || "",
+                cause: error.cause || ''
             })
         }
     }
@@ -147,17 +127,13 @@ export default class ProductManager {
         try {
             const m = await checkDb(this.path)
 
-            if (
-                updates.code &&
-                m.products.find((product) => product.code === updates.code)
-            )
+            if (updates.code && m.products.find((product) => product.code === updates.code))
                 throw em.createError({
-                    name: "CastError",
+                    name: 'CastError',
                     status: 400,
-                    message:
-                        "this code is already in other product, need a newone",
+                    message: 'this code is already in other product, need a newone',
                     code: ErrorCode.GENERAL_USER_ERROR,
-                    cause: "code must be unique",
+                    cause: 'code must be unique'
                 })
             if (m.products.find((p) => p.id === id)) {
                 let index = m.products.findIndex((p) => p.id === id)
@@ -170,29 +146,29 @@ export default class ProductManager {
                     this.path,
                     JSON.stringify({
                         products: m.products,
-                        payload: m.products.length,
+                        payload: m.products.length
                     })
                 )
                 return {
                     message: `  update at id : ${id} properly made`,
-                    content: pManager.products[index],
+                    content: m.products[index]
                 }
             } else {
                 throw em.createError({
-                    name: "CastError",
+                    name: 'CastError',
                     status: 404,
-                    message: "not product with the given id: " + id,
+                    message: 'not product with the given id: ' + id,
                     code: ErrorCode.GENERAL_USER_ERROR,
-                    cause: "product not found",
+                    cause: 'product not found'
                 })
             }
         } catch (error) {
             throw em.createError({
-                name: error.name || "Error",
-                message: error.message || "Error trying to get a product",
+                name: error.name || 'Error',
+                message: error.message || 'Error trying to get a product',
                 code: ErrorCode.DATABASE_ERROR,
                 status: 500,
-                cause: error.cause || "",
+                cause: error.cause || ''
             })
         }
     }
@@ -201,33 +177,31 @@ export default class ProductManager {
             const m = await checkDb(this.path)
             let product = m.products.find((p) => p.id.toString() === id)
             if (product) {
-                m.products = m.products.filter(
-                    (p) => p.id.toString() !== product.id
-                )
+                m.products = m.products.filter((p) => p.id.toString() !== product.id)
                 await fs.promises.writeFile(
                     this.path,
                     JSON.stringify({
                         products: m.products,
-                        payload: m.products.length,
+                        payload: m.products.length
                     })
                 )
-                return { message: "product deleted", content: product }
+                return { message: 'product deleted', content: product }
             } else {
                 throw em.createError({
-                    name: "CastError",
+                    name: 'CastError',
                     status: 404,
-                    message: "not product with the given id: " + id,
+                    message: 'not product with the given id: ' + id,
                     code: ErrorCode.GENERAL_USER_ERROR,
-                    cause: "not found",
+                    cause: 'not found'
                 })
             }
         } catch (error) {
             throw em.createError({
-                name: error.name || "Error",
-                message: error.message || "Error trying to get a product",
+                name: 'InternalError',
+                message: error.message || 'Error trying to get a product',
                 code: ErrorCode.DATABASE_ERROR,
                 status: 500,
-                cause: error.cause || "",
+                cause: error.cause || ''
             })
         }
     }
